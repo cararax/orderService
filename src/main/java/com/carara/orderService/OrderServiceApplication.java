3d6f84d081a9229f7513932bcc6b4282b149b4cc
Pedro Carara <cararax@gmail.com>
2022-02-09 18:01:41 -0300

    feat: instanciação e salva no banco de dados pedidos com clientes

--- src/main/java/com/carara/orderService/OrderServiceApplication.java
+++ src/main/java/com/carara/orderService/OrderServiceApplication.java
@@ -1,14 +1,18 @@
 package com.carara.orderService;
 
 import com.carara.orderService.entities.ClientModel;
+import com.carara.orderService.entities.OrderModel;
+import com.carara.orderService.entities.OrderStatusEnum;
 import com.carara.orderService.entities.ProductModel;
 import com.carara.orderService.repositories.ClientRepository;
+import com.carara.orderService.repositories.OrderRepository;
 import com.carara.orderService.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -18,6 +22,8 @@ public class OrderServiceApplication implements CommandLineRunner {
     ClientRepository clientRepository;
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    OrderRepository orderRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
@@ -28,10 +34,14 @@ public class OrderServiceApplication implements CommandLineRunner {
         ClientModel cliente1 = new ClientModel(null, "Alex Green", "alex@gmail.com");
         clientRepository.save(cliente1);
 
-        ProductModel p1 = new ProductModel(null, "TV", 1000.0);
-        ProductModel p2 = new ProductModel(null, "Mouse", 40.0);
-        ProductModel p3 = new ProductModel(null, "PC", 1200.0);
+        ProductModel product1 = new ProductModel(null, "TV", 1000.0);
+        ProductModel product2 = new ProductModel(null, "Mouse", 40.0);
+        ProductModel product3 = new ProductModel(null, "PC", 1200.0);
+        productRepository.saveAll(Arrays.asList(product1, product2, product3));
 
-        productRepository.saveAll(Arrays.asList(p1, p2, p3));
+        OrderModel order1 = new OrderModel(null, Instant.parse("2022-02-09T11:25:09Z"), OrderStatusEnum.PAID, cliente1);
+        OrderModel order2 = new OrderModel(null, Instant.parse("2022-02-09T13:30:00Z"), OrderStatusEnum.WAITING, cliente1);
+        orderRepository.saveAll(Arrays.asList(order1, order2));
+
     }
 }
